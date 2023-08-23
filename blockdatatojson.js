e5deff8adfb6d24068b3888da2e0e261a8db18e6
DoomKitty87 <doomkitty887@gmail.com>
2023-08-22 17:52:45 -0700

    sectorized block data for speed

--- blockdatatojson.js
+++ blockdatatojson.js
@@ -22,3 +22,43 @@ for (var x = 0; x < 622; x++) {
 }
 var blockDataOut = JSON.stringify(blockData);
 fs.writeFileSync('blockarraydata.json', blockDataOut);
+
+const jadecoordsx = 823;
+const jadecoordsz = 202;
+
+const sectors = [];
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
+    const sectortmp = [];
+    const cornerx = jadecoordsx + i * -128;
+    const cornerz = jadecoordsz + j * 128;
+    sectortmp.push(cornerx);
+    sectortmp.push(cornerz);
+    sectortmp.push(cornerx - 128);
+    sectortmp.push(cornerz + 128);
+    sectors.push(sectortmp);
+  }
+}
+
+for (let sec = 0; sec < 25; sec++) {
+  if (sec == 12) continue;
+  const sector = sectors[sec];
+  const sectorx = sector[0];
+  const sectorz = sector[1];
+  const secData = [];
+  secData.length = 128;
+  for (let x = 0; x < 128; x++) {
+    secData[x] = [];
+    secData[x].length = 256;
+    for (let y = 0; y < 256; y++) {
+      secData[x][y] = [];
+      secData[x][y].length = 128;
+      for (let z = 0; z < 128; z++) {
+        if (sectorx - x < 0 || sectorx - x > 621 || sectorz + z > 621) secData[x][y][z] = 0;
+        else secData[x][y][z] = blockData[sectorx - x][y][sectorz + z];
+      }
+    }
+  }
+  const secDataOut = JSON.stringify(secData);
+  fs.writeFileSync(`sector${sec}.json`, secDataOut);
+}
