bf19d002ca5f45d5013ac41e1a72b8e3c4d24a8c
DoomKitty87 <doomkitty887@gmail.com>
2023-08-25 15:33:57 -0700

    added stuff for ore exporting

---
+++ oreextract.py
@@ -0,0 +1,34 @@
+import anvil
+import json
+
+files = ["r.0.0.mca2", "r.0.1.mca2", "r.1.0.mca2", "r.1.1.mca2"]
+
+blocks = []
+
+with open("iron.json", "w") as f:
+  f.write("")
+
+for f in files:
+    region = anvil.Region.from_file(f)
+    regioncoords = f.split(".")
+    for cx in range(32):
+      print(len(blocks))
+      for cz in range(32):
+        xchunk = cx + int(regioncoords[1]) * 32
+        zchunk = cz + int(regioncoords[2]) * 32
+        chunk = region.get_chunk(xchunk, zchunk)
+        if chunk == anvil.EmptyChunk(xchunk, zchunk): continue
+        for x in range(16):
+          for y in range(256):
+            for z in range(16):
+              # print(chunk.get_block(x, y, z).id)
+              if ((x + xchunk * 16 > 460 and x + xchunk * 16 < 565) and (z + zchunk * 16 > 460 and z + zchunk * 16 < 565)): continue
+              if chunk.get_block(x, y, z).id == 15:
+                blocks.append([x + xchunk * 16, y, z + zchunk * 16])
+
+#Changed from per-cycle due to the issue of parsing an indeterminate amount of json objects.
+#Could also just save it every cycle, or every n cycles.
+with open("iron.json", "a") as f:
+  print(str(len(blocks)) + " blocks")
+  data = json.dumps(blocks)
+  f.write(data)
