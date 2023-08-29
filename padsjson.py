eff89fd3e25b11a4c820428d37d12458998fe657
DoomKitty87 <doomkitty887@gmail.com>
2023-08-29 15:36:14 -0700

    changes

--- padsjson.py
+++ padsjson.py
@@ -16,8 +16,10 @@ for b in blocks:
 pads = []
 
 for x in range(0, 622):
+  print(x)
   for y in range(0, 254):
     for z in range(0, 622):
+      if (x > 460 and x < 565 and z > 460 and z < 565): continue
       if (blockData[x][y + 1][z] == 0 and blockData[x][y + 2][z] == 0):
         density = 0
         for dx in range(-1, 2):
@@ -25,7 +27,7 @@ for x in range(0, 622):
             for dz in range(-1, 2):
               if (x + dx >= 0 and x + dx < 622 and y + dy >= 0 and y + dy < 256 and z + dz >= 0 and z + dz < 622):
                 density += blockData[x + dx][y + dy][z + dz]
-        if density > 15:
+        if density > 10:
           pads.append({"x": x + 202, "y": y, "z": z + 202, "density": density * 3})
 
 with open('rubyhybrid.json', 'w') as f:
