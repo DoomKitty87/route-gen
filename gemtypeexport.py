1bf094c2a18929ef6e156319981545950a2444c2
DoomKitty87 <doomkitty887@gmail.com>
2023-08-29 08:45:54 -0700

    added redstone

--- gemtypeexport.py
+++ gemtypeexport.py
@@ -7,11 +7,14 @@ for x in range(202, 824):
   for y in range(0, 256):
     for z in range(202, 824):
       block = level.get_block(x, y, z, "minecraft:overworld")
-      if (block.base_name == "stained_glass" or block.base_name == "stained_glass_pane"):
+      #if (block.base_name == "stained_glass" or block.base_name == "stained_glass_pane"):
         #print(level.get_block(x, y, z, "minecraft:overworld").properties['color'])
-        if (block.properties['color'] == "yellow"):
-          blocks.append([x, y, z])
+        #if (block.properties['color'] == "yellow"):
+          #blocks.append([x, y, z])
           #print(len(blocks))
-with open('topaz.json', 'w') as f:
+      if (block.base_name == "redstone_ore"):
+        blocks.append([x, y, z])
+        #print(len(blocks))
+with open('redstone.json', 'w') as f:
   json.dump(blocks, f)
 level.close()
